f72ec382ea96d7767c17b4785e052990e362d108
sriharsha <91383946+sriharsha8991@users.noreply.github.com>
2024-02-14 12:47:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 from PIL import Image
 import google.generativeai as genai
 
-genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key = "AIzaSyBIPucZCQr14TZm9DKfA0ACl0DXLRq2JeE")
 
 #function to load Gemini Pro Vision
 model = genai.GenerativeModel('gemini-pro-vision')
